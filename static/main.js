2a5b99c4bc294094c93325b5afd02c0957cae472
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2022-05-09 11:42:51 -0400

    Add files via upload

--- static/main.js
+++ static/main.js
@@ -1,7 +1,9 @@
-/* document.getElementById('btn').addEventListener('click', () => {
-    document.getElementById('top').style.display = 'block';
+document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('top').classList.toggle('show')
+    /* .style.display = 'block';
+    document.querySelector('.nav-menu').classList.toggle('show'); */
 });
 
-document.getElementById('blog').addEventListener('click', () => {
+/* document.getElementById('blog').addEventListener('click', () => {
     document.getElementById('top').style.display = 'none'
 }); */
